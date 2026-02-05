73fd0f872b66c67ffff4a019a95053b5b7d56870
Valentin <91955476+Valentin-Grachev@users.noreply.github.com>
2026-02-05 11:39:15 +0300

    gp fix

--- Assets/- Project -/Context/GamePush/ProjectData.cs
+++ Assets/- Project -/Context/GamePush/ProjectData.cs
@@ -3,7 +3,7 @@ namespace GamePush.Data
     public static class ProjectData
     {
         public static string SDK_VERSION = "1.8.1";
-        public static string ID = "";
+        public static string ID = "0";
         public static string TOKEN = "";
         public static bool GAMEREADY_AUTOCALL = false;
         public static bool SHOW_STICKY_ON_START = false;
--- Packages/com.devnote.sdk.game_push/Runtime/Plugins/GamePush/JS/_dataFields.jspre
+++ Packages/com.devnote.sdk.game_push/Runtime/Plugins/GamePush/JS/_dataFields.jspre
@@ -1,3 +1,3 @@
-const dataProjectId = '5682';
+const dataProjectId = '5680';
 const dataPublicToken = 'RCoEo3udCIZaTCASqNz6sZP6gLJ9JGbb';
 const showPreloaderAd = 'False';
